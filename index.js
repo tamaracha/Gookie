12d3b46f5f9e6b41fd6523f1243cc0fa528d1d07
Tamara Cook <Tamara.Cook@icloud.com>
2015-08-12 16:58:53 +0200

    use githubhook

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-'use strict';
-const koa=require('koa');
-const router=new require('koa-router')();
-const app=koa();
-require('koa-onerror')(app);
-const config=require('config');
-const server=config.get('server');
-const parseBody = require('./middleware/parse-body');
-const validateRequest = require('./middleware/validate-request');
-const githubToken = require('./middleware/github-token');
-const deploy = require('./middleware/deploy');
-
-router.get('/',function *(){
-  this.status=200;
+const githubhook = require('githubhook');
+const github = githubhook({
+  host: '127.0.0.1',
+  port: 9000,
+  path: '/',
+  logger: console
 });
-router.post('/',
-  parseBody,
-  validateRequest,
-  githubToken,
-  function *(){
-    this.status=204;
-  }
-);
-app.use(router.routes())
-.use(router.allowedMethods())
-.listen(server.port,server.host,function(){
-  console.log(`listening on ${server.host}:${server.port}`);
+github.listen();
+github.on('*',function(e,repo){
+  console.log(e);
 });
