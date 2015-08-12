104147c441c4ae0d5706b2ca341574545a06f1cb
Tamara Cook <Tamara.Cook@icloud.com>
2015-08-12 18:11:46 +0200

    added handler

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+const child = require('child_process');
 const githubhook = require('githubhook');
 const config = require('config');
 const server = config.get('server');
+const repos = config.get('repositories');
 server.logger = console;
 const github = githubhook(server);
 github.listen();
 github.on('push',function(repo,ref,data){
-  console.log({repo, ref, data});
+  const repoConfig = repos[repo];
+  if(!repoConfig){
+    return;
+  }
+  const command = `cd ${repoconfig.path} && ${repoConfig.deploy}`;
+  return child.exec(command,function(err,result){
+    if(err){return err;}
+    if(result.stdout){
+      console.log(result.stdout);
+    }
+    if(result.stderr){
+      console.error(result.stderr);
+    }
+  });
 });
