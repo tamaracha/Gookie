0c97d7033cbb329ce3198c6309bd21cd53ec4c4c
Tamara Cook <Tamara.Cook@icloud.com>
2015-09-02 20:42:33 +0200

    back to child_process

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 'use strict';
-const pm2 = require('pm2');
+const child = require('child_process');
 const githubhook = require('githubhook');
 const config = require('config');
 const server = config.get('server');
 const repos = config.get('repositories');
 const github = githubhook(server);
 github.listen();
-function pushHandler(repo,ref,data){
+github.on('push',function(repo,ref,data){
   const repoConfig = repos[repo];
   if(!repoConfig){
     return;
   }
-  pm2.connect(function(){
-    pm2.pullAndRestart(repo,function(err,out){
-      if(err){
-        console.error(err);
-      }
-      console.log(out);
-      pm2.disconnect();
-    });
+  child.exec(repoConfig.deploy,{cwd: repoConfig.path, env: {}},function(err,stdout,stderr){
+    if(err){
+      console.error(err);
+    }
+    if(stdout){
+      console.log(stdout);
+    }
+    if(stderr){
+      console.warn(stderr);
+    }
   });
-}
-github.on('push',pushHandler);
+});
